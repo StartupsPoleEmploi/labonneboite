97df78a21305830d894115756233d38cc3fa4e4b
Vermeer GRANGE <vermeer.grange@gmail.com>
2017-08-24 12:08:07 +0200

    vermeer's first commit

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get --assume-yes install git
 apt-get --assume-yes install python
-apt-get --assume-yes install python-pip 
+apt-get --assume-yes install python-pip
 apt-get --assume-yes install libmariadbclient-dev mariadb-server mariadb-client-core-5.5
 apt-get install -qy python-dev
 apt-get install -qy libssl-dev
