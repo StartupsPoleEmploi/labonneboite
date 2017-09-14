e78b127ceb8cb093bce2ce0be8bfb6b85483e9fb
Michel Blancard <michel.blancard@data.gouv.fr>
2017-09-14 18:14:31 +0200

    Remove legacy code in ROME_NAF/print_array.py

--- ROME_NAF/analyses/print_array.py
+++ ROME_NAF/analyses/print_array.py
@@ -10,10 +10,8 @@ stats_lbb = pd.read_csv('../stats_lbb/stats_lbb.csv')
 top_slugs = stats_lbb[:34]
 
 def pprint_ROME(array):
-    i = 0
-    for l in top_slugs.iterrows():
-        slugs, visite, uniq = l[1]
-        i += 1
+    for i, line in enumerate(top_slugs.iterrows()):
+        slugs, visite, uniq = line[1]
 
         assert len(ROME_df[ROME_df.slugs == slugs]) == 1
 
@@ -29,112 +27,5 @@ def pprint_ROME(array):
             NAF_code = NAF_df.iloc[NAF_index].name
             NAF_label = NAF_df.loc[NAF_code].label
 
-            #print('{}, {} : {}\n    {}'.format(NAF, NAF_label, poids_NAF[NAF_index], str(array[NAF_index, ROME_index, :])))
             print('{}, {} : {}'.format(NAF_code, NAF_label, poids_NAF[NAF_index], ))
-        #print(' '.join([str(NAF) for NAF in top_NAFs]))
         print()
-
-
-    
-# Sortie CSV pour mise en prod
-
-"""
-with open('array_ROME1.pickle', 'rb') as f:
-    array_ROME1 = pickle.load(f)
-with open('array_ROME2.pickle', 'rb') as f:
-    array_ROME2 = pickle.load(f)
-    
-def anonymise(x):
-    if 0 < x and x < 10:
-        return '?'
-    else:
-        return str(x)
-
-def write_line_lbb(array, ROME_code, NAF_code, f):
-    ROME_index = ROME_df.index.get_loc(ROME_code)
-    NAF_index = NAF_df.index.get_loc(NAF_code)
-    
-    sum_for_ROME_NAF = array[NAF_index, ROME_index, :].sum()
-    
-    if sum_for_ROME_NAF > 0:
-        str_to_write = '{},{},"{}","{}",{},,,,'.format(
-            ROME_code,
-            NAF_code,
-            NAF_df.loc[NAF_code].label,
-            ROME_df.loc[ROME_code].label,
-            sum_for_ROME_NAF,
-            )
-
-        str_to_write += ','.join([
-            str(array[NAF_index, ROME_index, tranche_index])
-            for tranche_index, tranche in 
-            enumerate(tranches_effectif)
-        ])
-
-        str_to_write += '\n'
-
-        f.write(str_to_write)
-        
-def write_line_opendata(array, ROME_code, NAF_code, f):
-    ROME_index = ROME_df.index.get_loc(ROME_code)
-    NAF_index = NAF_df.index.get_loc(NAF_code)
-    
-    sum_for_ROME_NAF = array[NAF_index, ROME_index, :].sum()
-    
-    if sum_for_ROME_NAF > 0:
-        str_to_write = '{},{},"{}","{}",{},'.format(
-            ROME_code,
-            NAF_code,
-            ROME_df.loc[ROME_code].label,
-            NAF_df.loc[NAF_code].label,
-            str(int(sum_for_ROME_NAF)),
-            )
-
-        str_to_write += ','.join([
-            str(int(array[NAF_index, ROME_index, tranche_index]))
-            for tranche_index, tranche in 
-            enumerate(tranches_effectif)
-        ])
-
-        str_to_write += '\n'
-
-        f.write(str_to_write)
-        
-with open('mapping_ROME1_NAF_from_DPAE_lbb.csv', 'w') as f:
-    for ROME_code in ROME_df.index:
-        for NAF_code in NAF_df.index:
-            write_line_lbb(array_ROME1, ROME_code, NAF_code, f) 
-            
-with open('mapping_ROME2_NAF_from_DPAE_lbb.csv', 'w') as f:
-    for ROME_code in ROME_df.index:
-        for NAF_code in NAF_df.index:
-            write_line_lbb(array_ROME2, ROME_code, NAF_code, f) 
-            
-with open('mapping_ROME1_NAF_from_DPAE_opendata.csv', 'w') as f:
-    header = 'ROME_code,NAF_code,ROME_label,NAF_label,total,'
-    header += ','.join([
-        'TEFET_' + tranche
-        for tranche in 
-        tranches_effectif
-        ])
-    header += '\n'
-    f.write(header)
-    
-    for ROME_code in ROME_df.index:
-        for NAF_code in NAF_df.index:
-            write_line_opendata(array_ROME1, ROME_code, NAF_code, f)
-
-with open('mapping_ROME2_NAF_from_DPAE_opendata.csv', 'w') as f:
-    header = 'ROME_code,NAF_code,ROME_label,NAF_label,total,'
-    header += ','.join([
-        'TEFET_' + tranche
-        for tranche in 
-        tranches_effectif
-        ])
-    header += '\n'
-    f.write(header)
-    
-    for ROME_code in ROME_df.index:
-        for NAF_code in NAF_df.index:
-            write_line_opendata(array_ROME2, ROME_code, NAF_code, f)
-"""
