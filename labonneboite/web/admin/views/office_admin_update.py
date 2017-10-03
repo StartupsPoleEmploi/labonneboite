2e1b335d234bd9e870591dc3082e09830fb519ad
AlexandreCantin <cantin.alexandre@gmail.com>
2017-10-03 19:01:22 +0200

    Add the form field for "romes_to_remove" in the back-office

--- labonneboite/web/admin/views/office_admin_update.py
+++ labonneboite/web/admin/views/office_admin_update.py
@@ -34,6 +34,7 @@ class OfficeAdminUpdateModelView(AdminModelViewMixin, ModelView):
         'name',
         'boost',
         'romes_to_boost',
+        'romes_to_remove',
         'new_email',
         'new_phone',
         'new_website',
@@ -54,7 +55,8 @@ class OfficeAdminUpdateModelView(AdminModelViewMixin, ModelView):
     column_formatters = {
         'date_created': datetime_format,
         'date_updated': datetime_format,
-        'romes_to_boost': lambda view, context, model, name: Markup(model.romes_to_boost_as_html()),
+        'romes_to_boost': lambda view, context, model, name: Markup(model.romes_as_html(model.romes_to_boost)),
+        'romes_to_remove': lambda view, context, model, name: Markup(model.romes_as_html(model.romes_to_remove)),
     }
 
     column_labels = {
@@ -63,6 +65,7 @@ class OfficeAdminUpdateModelView(AdminModelViewMixin, ModelView):
         'reason': u"Raison",
         'boost': u"Booster le score",
         'romes_to_boost': u"Limiter le boosting du score à certain codes ROME uniquement",
+        'romes_to_remove': u"Retirer des codes ROME associés à une entreprise",
         'new_email': u"Nouvel email",
         'new_phone': u"Nouveau téléphone",
         'new_website': u"Nouveau site web",
@@ -92,6 +95,13 @@ class OfficeAdminUpdateModelView(AdminModelViewMixin, ModelView):
             u"<br>"
             u"<a href=\"/data/romes-for-siret\" target=\"_blank\">Trouver les ROME pour un SIRET</a>."
         ),
+        'romes_to_remove': Markup(
+            u"Veuillez entrer un ROME par ligne."
+            u"<br>"
+            u"Si ce champ est renseigné, le(s) ROME spécifié(s) seront retirés pour cette entreprise."
+            u"<br>"
+            u"<a href=\"/data/romes-for-siret\" target=\"_blank\">Trouver les ROME pour un SIRET</a>."
+        ),
         'new_email': u"Laisser vide s'il n'y a pas de modification à apporter",
         'new_phone': u"Laisser vide s'il n'y a pas de modification à apporter",
         'new_website': u"Laisser vide s'il n'y a pas de modification à apporter",
@@ -106,6 +116,7 @@ class OfficeAdminUpdateModelView(AdminModelViewMixin, ModelView):
         'name',
         'boost',
         'romes_to_boost',
+        'romes_to_remove',
         'new_email',
         'new_phone',
         'new_website',
@@ -128,6 +139,9 @@ class OfficeAdminUpdateModelView(AdminModelViewMixin, ModelView):
             'filters': [strip_filter],
         },
         'romes_to_boost': {
+            'filters': [strip_filter],
+        },
+        'romes_to_remove': {
             'filters': [strip_filter],
         },
         'new_email': {
@@ -162,6 +176,7 @@ class OfficeAdminUpdateModelView(AdminModelViewMixin, ModelView):
     def validate_form(self, form):
         is_valid = super(OfficeAdminUpdateModelView, self).validate_form(form)
 
+        # Code ROMES to boost or to add
         if is_valid and form.data.get('romes_to_boost'):
 
             romes_to_boost = form.data.get('romes_to_boost')
@@ -188,5 +203,35 @@ class OfficeAdminUpdateModelView(AdminModelViewMixin, ModelView):
                     )
                     flash(Markup(msg), 'error')
                     return False
+
+        # Codes ROMES to remove
+        if is_valid and form.data.get('romes_to_remove'):
+            romes_to_remove = form.data.get('romes_to_remove')
+
+            office_to_update = Office.query.filter_by(siret=form.data['siret']).first()
+            if not office_to_update:
+                msg = u"Le siret `%s` n'est pas présent sur LBB." % form.data['siret']
+                flash(msg, 'error')
+                return False
+
+            mapper = mapping_util.Rome2NafMapper()
+            office_romes = [item.code for item in mapper.romes_for_naf(office_to_update.naf)]
+            for rome in OfficeAdminUpdate.romes_as_list(romes_to_remove):
+
+                if not mapper.romes_is_valid(rome):
+                    msg = (
+                        u"`%s` n'est pas un code ROME valide."
+                        u"<br>"
+                        u"Assurez-vous de ne saisir qu'un élément par ligne."
+                        % rome
+                    )
+                    flash(Markup(msg), 'error')
+                    return False
+
+                if rome not in office_romes:
+                    msg = u"`%s` n'est pas un code ROME lié au NAF de cette entreprise." % rome
+                    flash(msg, 'error')
+                    return False
+
 
         return is_valid
