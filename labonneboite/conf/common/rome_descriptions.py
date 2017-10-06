338b1a9f5eacc9d1dad19d08a0347a98ff154ac0
Vermeer GRANGE <vermeer.grange@gmail.com>
2017-10-06 10:41:09 +0200

    minor cleanup

--- labonneboite/conf/common/rome_descriptions.py
+++ labonneboite/conf/common/rome_descriptions.py
@@ -1,9 +1,3 @@
 # coding: utf8
 from labonneboite.common.load_data import load_rome_labels
 ROME_DESCRIPTIONS = load_rome_labels()
-
-# import os
-# filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'xxxxxxxxxx.csv')
-# with open(filename, 'w') as f:
-# 	for k in ROME_DESCRIPTIONS:
-# 		f.write("%s,%s\n" % (k, ROME_DESCRIPTIONS[k].encode('utf-8')))
