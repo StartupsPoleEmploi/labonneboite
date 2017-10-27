01706e8d2939359c8def877f99fbe7bdae2ec9c5
AlexandreCantin <cantin.alexandre@gmail.com>
2017-10-27 17:46:04 +0200

    Make the form more accessible

--- labonneboite/web/office/forms.py
+++ labonneboite/web/office/forms.py
@@ -16,19 +16,20 @@ class OfficeRemovalForm(FlaskForm):
         (u'autre', u'Autre')
     )
 
-    action = SelectField(u'Je souhaite :', choices=ACTION_CHOICES, default=u'promouvoir')
+    action = SelectField(u'Je souhaite *', choices=ACTION_CHOICES, default=u'promouvoir')
     siret = StringField(
-        u'Siret (14 chiffres)',
+        u'Siret *',
         validators=[
             DataRequired(),
             Regexp(
                 '[0-9]{10,14}',
                 message=(u"Le siret de l'établissement dont vous êtes le dirigeant est invalide (14 chiffres)")
             )
-        ]
+        ],
+        description="14 chiffres, sans espace. Exemple: 36252187900034"
     )
-    first_name = StringField(u'Prénom', validators=[DataRequired()])
-    last_name = StringField(u'Nom', validators=[DataRequired()])
-    email = EmailField(u'Email', validators=[DataRequired(), Email()])
-    phone = TelField(u'Téléphone')
-    comment = TextAreaField(u'Commentaires', [validators.optional(), validators.length(max=200)])
+    first_name = StringField(u'Prénom *', validators=[DataRequired()])
+    last_name = StringField(u'Nom *', validators=[DataRequired()])
+    email = EmailField(u'Email *', validators=[DataRequired(), Email()], description="Exemple : example@domaine.com")
+    phone = TelField(u'Téléphone', description="Exemples: 01 77 86 39 49, +33 1 77 86 39 49")
+    comment = TextAreaField(u'Commentaires', [validators.optional(), validators.length(max=1000)], description=u"1000 caractères maximum")
