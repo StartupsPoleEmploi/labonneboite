08529a5636c414ab97c6fa32ea3f93ed09aae357
AlexandreCantin <cantin.alexandre@gmail.com>
2017-11-20 11:59:02 +0100

    Update migration for email_alternance

--- labonneboite/alembic/versions/11fc7f39f7fc_create_email_alternance_column.py
+++ labonneboite/alembic/versions/11fc7f39f7fc_create_email_alternance_column.py
@@ -1,9 +1,9 @@
 """
-Add email_alternance column
+create email_alternance column
 
-Revision ID: fecea26e74c9
-Revises: bde9330b83fa
-Create Date: 2017-11-14 11:48:38.397980
+Revision ID: 11fc7f39f7fc
+Revises: 0592646101eb
+Create Date: 2017-11-20 11:19:59.347665
 """
 from alembic import op
 from sqlalchemy.dialects import mysql
@@ -11,8 +11,8 @@ import sqlalchemy as sa
 
 
 # Revision identifiers, used by Alembic.
-revision = 'fecea26e74c9'
-down_revision = 'bde9330b83fa'
+revision = '11fc7f39f7fc'
+down_revision = '0592646101eb'
 branch_labels = None
 depends_on = None
 
@@ -20,7 +20,9 @@ depends_on = None
 def upgrade():
     op.add_column('etablissements_admin_update', sa.Column('email_alternance', mysql.TEXT(collation=u'utf8mb4_unicode_ci'), nullable=False))
     op.add_column('etablissements_admin_add', sa.Column('email_alternance', mysql.TEXT(collation=u'utf8mb4_unicode_ci'), nullable=False))
+    op.add_column('etablissements_exportable', sa.Column('email_alternance', mysql.TEXT(collation=u'utf8mb4_unicode_ci'), nullable=False))
 
 def downgrade():
     op.drop_column('etablissements_admin_update', 'email_alternance')
     op.drop_column('etablissements_admin_add', 'email_alternance')
+    op.drop_column('etablissements_exportable', 'email_alternance')
