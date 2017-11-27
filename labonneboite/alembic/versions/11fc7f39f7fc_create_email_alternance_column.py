e0c02f012ba869a98a20351e422d935007ee52b2
AlexandreCantin <cantin.alexandre@gmail.com>
2017-11-27 14:45:06 +0100

    Add etablissements_raw in migration

--- labonneboite/alembic/versions/11fc7f39f7fc_create_email_alternance_column.py
+++ labonneboite/alembic/versions/11fc7f39f7fc_create_email_alternance_column.py
@@ -21,8 +21,10 @@ def upgrade():
     op.add_column('etablissements_admin_update', sa.Column('email_alternance', mysql.TEXT(collation=u'utf8mb4_unicode_ci'), nullable=False))
     op.add_column('etablissements_admin_add', sa.Column('email_alternance', mysql.TEXT(collation=u'utf8mb4_unicode_ci'), nullable=False))
     op.add_column('etablissements_exportable', sa.Column('email_alternance', mysql.TEXT(collation=u'utf8mb4_unicode_ci'), nullable=False))
+    op.add_column('etablissements_raw', sa.Column('email_alternance', mysql.TEXT(collation=u'utf8mb4_unicode_ci'), nullable=False))
 
 def downgrade():
     op.drop_column('etablissements_admin_update', 'email_alternance')
     op.drop_column('etablissements_admin_add', 'email_alternance')
     op.drop_column('etablissements_exportable', 'email_alternance')
+    op.drop_column('etablissements_raw', 'email_alternance')
