6c028e4e3a38bbb8f410c0283dd28df6e9c9df75
Vermeer GRANGE <vermeer.grange@gmail.com>
2017-12-12 10:04:50 +0100

    bugfix about removing office in backoffice

--- labonneboite/web/admin/views/office_admin_remove.py
+++ labonneboite/web/admin/views/office_admin_remove.py
@@ -128,7 +128,7 @@ class OfficeAdminRemoveModelView(AdminModelViewMixin, ModelView):
         """
         is_valid = super(OfficeAdminRemoveModelView, self).validate_form(form)
         if is_valid:
-            office_to_add = OfficeAdminAdd.query.filter_by(id=form.data['id']).first()
+            office_to_add = OfficeAdminAdd.query.filter_by(siret=form.data['siret']).first()
             if office_to_add:
                 # Use the link of the list view with a filter on the `siret`, because
                 # the delete button is missing on the edit and/or detail view.
