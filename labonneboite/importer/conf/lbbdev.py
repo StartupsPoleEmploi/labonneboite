d7a3c755d7e2c2445d1432f4deefcc184242ca86
Vermeer GRANGE <vermeer.grange@gmail.com>
2018-01-19 15:00:47 +0100

    no longer backup importer tables to lbbbackup, see explanation why

--- labonneboite/importer/conf/lbbdev.py
+++ labonneboite/importer/conf/lbbdev.py
@@ -1,7 +1,11 @@
 import os
 
-# --- importer table backuping process
-BACKUP_FIRST = True
+# --- importer table backuping process (DPAE + ETABLISSEMENTS)
+# As of Jan 2018 we no longer make backups in order to accelerate the process, and for the following reasons:
+# Exports are files on lbbbackup server, mounted via NFS to lbbdev. Thus, as original files
+# are stored on lbbbackup and as db tables are stored on lbbdev, data is already physically on two different
+# servers, which we can consider as a rough backup good enough for now.
+BACKUP_FIRST = False
 
 # --- importer input directory of DPAE and ETABLISSEMENT exports
 INPUT_SOURCE_FOLDER = '/srv/lbb/data'
