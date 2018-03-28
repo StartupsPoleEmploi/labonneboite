77c7fee9699b1ed1da5a806c60d0b169fda22180
Régis Behmo <regis@behmo.com>
2018-03-28 16:08:01 +0200

    Move location objects to dedicated module

---
+++ labonneboite/common/locations.py
@@ -0,0 +1,44 @@
+# coding: utf8
+import logging
+
+from labonneboite.common import geocoding
+
+
+logger = logging.getLogger('main')
+
+
+class Location(object):
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __repr__(self):
+        return "lon: {} - lat: {}".format(self.longitude, self.latitude)
+
+
+class CityLocation(object):
+
+    def __init__(self, zipcode, slug=''):
+        self.zipcode = zipcode
+
+        # Location attribute may be None if slug/zipcode combination is incorrect
+        self.location = None
+        self.slug = slug
+        self.name = slug.replace('-', ' ').capitalize()
+
+        city = geocoding.get_city_by_zipcode(self.zipcode, slug=slug)
+        if not city:
+            logger.debug("unable to retrieve a city for zipcode `%s` and slug `%s`", self.zipcode, self.slug)
+        else:
+            coordinates = city['coords']
+            self.location = Location(coordinates['lat'], coordinates['lon'])
+            self.slug = city['slug']
+            self.name = city['name']
+
+    @property
+    def is_location_correct(self):
+        return self.location is not None
+
+    @property
+    def full_name(self):
+        return '%s (%s)' % (self.name, self.zipcode)
--- labonneboite/tests/app/test_locations.py
+++ labonneboite/tests/app/test_locations.py
@@ -1,19 +1,19 @@
 # coding: utf8
 import unittest
 
-from labonneboite.common import search
+from labonneboite.common import locations
 
 
 class CityLocationTest(unittest.TestCase):
 
     def test_hyphenated_city_name(self):
-        city = search.CityLocation('19100', 'brive-la-gaillarde')
+        city = locations.CityLocation('19100', 'brive-la-gaillarde')
         self.assertEqual(city.name, 'Brive-la-Gaillarde')
 
     def test_unicode_city_name(self):
-        city = search.CityLocation('05100', 'briancon')
+        city = locations.CityLocation('05100', 'briancon')
         self.assertEqual(city.name, u'Briançon')
 
     def test_no_slug(self):
-        city = search.CityLocation('05100')
+        city = locations.CityLocation('05100')
         self.assertEqual(city.name, u'Briançon')
