450ab24090c9fa1f6156ee3005fd23566b61072f
Régis Behmo <regis@behmo.com>
2018-04-23 18:28:20 +0200

    Fix selenium tests

--- labonneboite/tests/selenium/test_reset_naf.py
+++ labonneboite/tests/selenium/test_reset_naf.py
@@ -1,5 +1,6 @@
 # coding: utf8
 import time
+import urlparse
 
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
@@ -17,19 +18,28 @@ class TestResetNaf(LbbSeleniumTestCase):
         url = self.url_for('search.results', city='metz', zipcode='57000', occupation='comptabilite')
         self.driver.get(url)
 
-        self.assertIn('/entreprises/metz-57000/comptabilite', self.driver.current_url)
-        self.assertNotIn('naf=', self.driver.current_url)
+        url = urlparse.urlparse(self.driver.current_url)
+        parameters = dict(urlparse.parse_qsl(url.query))
+        self.assertEqual('/entreprises', url.path)
+        self.assertEqual('comptabilite', parameters['occupation'])
+        self.assertEqual('metz', parameters['city']) # city parameter is defined on redirect
+        self.assertEqual('57000', parameters['zipcode'])
+        self.assertNotIn('naf', parameters)
 
         # Filter by NAF `Activités comptables` (`6920Z`).
         select = Select(self.driver.find_element_by_id('naf'))
         select.select_by_value(u'6920Z')
 
         # The form should be auto-submitted after an option has been selected.
-        self.assertIn('/entreprises/metz-57000/comptabilite', self.driver.current_url)
-        self.assertIn('naf=6920Z&d', self.driver.current_url)
+        url = urlparse.urlparse(self.driver.current_url)
+        parameters = dict(urlparse.parse_qsl(url.query))
+        self.assertEqual('/entreprises', url.path)
+        self.assertEqual('comptabilite', parameters['occupation'])
+        self.assertEqual('Metz (57000)', parameters['l']) # form value is now full city name
+        self.assertEqual('6920Z', parameters['naf'])
 
         # Perform another search on `boucher`.
-        job_input = self.driver.find_element_by_id('job')
+        job_input = self.driver.find_element_by_name('j')
         job_input.clear()  # Reset the previous `comptabilite` search term.
         job_input.send_keys('boucher')
         time.sleep(3)  # The `staging` is really slow and can take a long time to show results.
@@ -44,5 +54,9 @@ class TestResetNaf(LbbSeleniumTestCase):
         self.driver.find_element_by_css_selector('form.js-search-form div.form-search button').click()
 
         # The NAF filter should be reset.
-        self.assertIn('/entreprises/metz-57000/boucherie', self.driver.current_url)
-        self.assertNotIn('naf=', self.driver.current_url)
+        url = urlparse.urlparse(self.driver.current_url)
+        parameters = dict(urlparse.parse_qsl(url.query))
+        self.assertEqual('/entreprises', url.path)
+        self.assertEqual('boucherie', parameters['occupation'])
+        self.assertEqual('Metz (57000)', parameters['l']) # form value is now full city name
+        self.assertNotIn('naf', parameters)
