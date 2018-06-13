904a88852d8deed0abf809d879b15cd39b1534bc
Régis Behmo <regis@behmo.com>
2018-06-13 15:46:00 +0200

    Fix TypeError on login attempt from Tilkee
    The referrer may not always be defined. When None, it raises a
    TypeError.

--- labonneboite/web/tilkee/views.py
+++ labonneboite/web/tilkee/views.py
@@ -19,7 +19,7 @@ def upload():
         return 'Cette entreprise n\'existe pas', 400
 
     if not current_user.is_authenticated:
-        next_url = request.referrer + u'#company-{}'.format(company.siret)
+        next_url = request.referrer + u'#company-{}'.format(company.siret) if request.referrer else ''
         login_url = url_for('social.auth', backend='peam-openidconnect', next=next_url)
         return render_template("tilkee/login.html", login_url=login_url)
 
