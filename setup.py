4dfee72af954c29744e64606690a6a7b7ac8e4ca
Régis Behmo <regis@behmo.com>
2018-06-27 10:08:13 +0200

    Improve setup.py
    In particular, add python required version and fix author email address.
    (it's a team effort)

--- setup.py
+++ setup.py
@@ -6,19 +6,28 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "LaBonneBoite",
-    version = "0.1",
-    author = "Tommy Jarnac",
-    author_email = "tommy.jarnac@gmail.com",
-    description = (""),
+    name="LaBonneBoite",
+    version="0.1",
+    author="La Bonne Boite",
+    author_email="labonneboite@pole-emploi.fr",
+    description=(""),
     packages=['labonneboite',],
     include_package_data=True,
     long_description=read('README.md'),
     install_requires=[req for req in open('requirements.txt')],
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'create_index = labonneboite.scripts.create_index:run',
             'update_lbb_data = labonneboite.importer.importer:run'
         ],
-    }
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
