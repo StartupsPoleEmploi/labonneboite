510135f4b8c165f9911967c77a0addfa5ca4c7cb
Régis Behmo <regis@behmo.com>
2018-08-02 16:10:07 +0200

    Simplify PE-connect authentication backend
    It is not necessary to implement two different classes that both inherit
    from BaseOAuth2. Class PEAMOAuth2 was not used at all, so we grouped the
    code in a single class and deleted PEAMOAuth2.

--- labonneboite/web/auth/backends/peam.py
+++ labonneboite/web/auth/backends/peam.py
@@ -5,38 +5,27 @@ Provides user registration and login using PEAM (Pôle Emploi Access Management)
 import requests
 
 from social_core.exceptions import AuthUnreachableProvider
-from social_core.backends.oauth import BaseOAuth2
 from social_core.backends.open_id_connect import OpenIdConnectAuth
 
 from labonneboite.conf import settings
 from .exceptions import AuthFailedMissingReturnValues
 
 
-# We don't need to override some abstract methods in parent classes.
 # pylint:disable=abstract-method
-
-class PEAMOAuth2(BaseOAuth2):
-    """
-    Pôle Emploi Access Management OAuth2 backend.
-    """
-    name = 'peam-oauth2'
-
-    AUTHORIZATION_URL = '%s/connexion/oauth2/authorize' % settings.PEAM_AUTH_BASE_URL
-    ACCESS_TOKEN_URL = '%s/connexion/oauth2/access_token' % settings.PEAM_AUTH_BASE_URL
-    REFRESH_TOKEN_URL = '%s/connexion/oauth2/access_token' % settings.PEAM_AUTH_BASE_URL
-
-    def request_access_token(self, *args, **kwargs):
-        kwargs['params'] = {'realm': '/individu'}
-        return self.get_json(*args, **kwargs)
-
-
-class PEAMOpenIdConnect(PEAMOAuth2, OpenIdConnectAuth):
+class PEAMOpenIdConnect(OpenIdConnectAuth):
     """
     Pôle Emploi Access Management OpenId Connect backend.
     """
     name = 'peam-openidconnect'
 
+    AUTHORIZATION_URL = '%s/connexion/oauth2/authorize' % settings.PEAM_AUTH_BASE_URL
+    ACCESS_TOKEN_URL = '%s/connexion/oauth2/access_token' % settings.PEAM_AUTH_BASE_URL
+    REFRESH_TOKEN_URL = '%s/connexion/oauth2/access_token' % settings.PEAM_AUTH_BASE_URL
     USERINFO_URL = settings.PEAM_USERINFO_URL
+
+    def request_access_token(self, *args, **kwargs):
+        kwargs['params'] = {'realm': '/individu'}
+        return self.get_json(*args, **kwargs)
 
     def user_data(self, access_token, *args, **kwargs):
         url = self.userinfo_url()
@@ -64,10 +53,8 @@ class PEAMOpenIdConnect(PEAMOAuth2, OpenIdConnectAuth):
                 'external_id': response['sub'],
                 'gender': response['gender'],
                 'first_name': response['given_name'],
-                'last_name': response['family_name'] if 'family_name' in response else None,
+                'last_name': response.get('family_name'),
             }
         except KeyError as e:
             # Sometimes PEAM responds without the user details.
             raise AuthFailedMissingReturnValues(*e.args)
-
-# pylint:enable=abstract-method
