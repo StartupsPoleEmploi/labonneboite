2a71b48fb3ff2ec720ace74e30a83102c31863dc
AlexandreCantin <cantin.alexandre@gmail.com>
2018-09-19 11:43:36 +0200

    Handle HttpError when sending email

--- labonneboite/common/email_util.py
+++ labonneboite/common/email_util.py
@@ -2,6 +2,7 @@
 
 import json
 import logging
+from urllib.error import HTTPError
 
 from labonneboite.conf import settings
 
@@ -26,12 +27,17 @@ class MandrillClient(EmailClient):
     def send(self, html):
         from_email = self.from_email
         to_email = self.to
-        response = self.mandrill.send_email(
-            subject=self.subject,
-            to=[{'email': to_email}],
-            html=html,
-            from_email=from_email)
-        content = json.loads(response.content.decode())
-        if content[0]["status"] != "sent":
+
+        try:
+            response = self.mandrill.send_email(
+                subject=self.subject,
+                to=[{'email': to_email}],
+                html=html,
+                from_email=from_email)
+            content = json.loads(response.content.decode())
+            if content[0]["status"] != "sent":
+                raise MailNoSendException("email was not sent from %s to %s" % (from_email, to_email))
+        except HTTPError:
             raise MailNoSendException("email was not sent from %s to %s" % (from_email, to_email))
+
         return response
