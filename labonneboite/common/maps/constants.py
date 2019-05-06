ee7588db735c048cbd8bf5aa3618e20ed055799b
Régis Behmo <regis@behmo.com>
2019-05-06 12:48:20 +0200

    Isochronie forms redesign
    We redesigned the distance/duration search forms to maek them easier to
    understand: it did not make much sense to offer the user to search both
    by distance and duration. So we display dynamic input fields, either
    distance or duration, with a nice switch.

--- labonneboite/common/maps/constants.py
+++ labonneboite/common/maps/constants.py
@@ -6,11 +6,11 @@ PUBLIC_MODE = 'public'
 DEFAULT_TRAVEL_MODE = CAR_MODE
 
 TRAVEL_MODES = (
-    CAR_MODE,
     PUBLIC_MODE,
+    CAR_MODE,
 )
 
 TRAVEL_MODES_FRENCH = {
-    CAR_MODE: 'voiture',
-    PUBLIC_MODE: 'transports en commun',
+    CAR_MODE: 'Voiture',
+    PUBLIC_MODE: 'Transports en commun',
 }
