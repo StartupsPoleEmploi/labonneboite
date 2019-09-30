71c6acf9d7c23c88a530a631ead5d34d07adf7eb
Vermeer GRANGE <vermeer.grange@gmail.com>
2019-09-30 14:24:17 +0200

    (MacOS) Make selenium tests run headless Before this change, chrome would open windows in foreground during local selenium tests, annoying the developer. Found here: https://stackoverflow.com/questions/53657215/running-selenium-with-headless-chrome-webdriver

--- labonneboite/tests/selenium/base.py
+++ labonneboite/tests/selenium/base.py
@@ -5,6 +5,7 @@ import easyprocess
 from flask import url_for as flask_url_for
 from flask_testing import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from labonneboite.conf import settings
@@ -63,9 +64,15 @@ class LbbSeleniumTestCase(LiveServerTestCase):
         capabilities = DesiredCapabilities.CHROME
         capabilities['loggingPrefs'] = {'browser': 'ALL'}
 
+        chrome_options = Options()
+        #chrome_options.add_argument("--disable-extensions")
+        #chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--headless")
+
         self.driver = webdriver.Chrome(
             desired_capabilities=capabilities,
-            executable_path=chromedriver_path
+            executable_path=chromedriver_path,
+            options=chrome_options,
         )
 
         # Ensure that the window size is large enough so that HTML elements won't overlap.
