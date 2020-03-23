1699ac5e8d0c3fc9048419e136b034acbc3ea6a7
jimmydore <jimmy291295@gmail.com>
2020-03-23 17:23:58 +0100

    Fix the field used to compute number of employees in an office

--- labonneboite/common/load_data.py
+++ labonneboite/common/load_data.py
@@ -1,6 +1,8 @@
 import os
 import pickle
 import csv
+import pandas as pd
+import math
 
 from functools import lru_cache
 from collections import defaultdict
@@ -28,6 +30,11 @@ def load_pickle_file(filename):
         return pickle.load(open(full_filename, "r"))
     return load_file(f, filename)
 
+def load_pd_dataframe(filename, delimiter='', dtype=None):
+    def f(full_filename):
+        return pd.read_csv(open(full_filename, "r"), dtype=dtype)
+
+    return load_file(f, filename)
 
 def load_csv_file(filename, delimiter='|'):
     def f(full_filename):
@@ -193,5 +200,53 @@ def load_siret_to_remove():
 
     return sirets_to_remove
 
+#Used by importer job to extract etablissement
+@lru_cache(maxsize=None)
+def load_effectif_labels():
+    '''
+    Dataframe to load look like this.
+        code      label
+    0      0        0-0
+    1      1        1-2
+    2      2        3-5
+    3      3        6-9
+    4     11      10-19
+    5     12      20-49
+    6     21      50-99
+    7     22    100-199
+    8     31    200-249
+    9     32    250-499
+    10    41    500-999
+    11    42  1000-1999
+    12    51  2000-4999
+    13    52  5000-9999
+    14    53     10000+
+
+    '''
+    def create_column(row, which='start_effectif'):
+        '''
+        From the label, we want to create a start and end column to delimitate the interval
+        We'll be able to use it to simply determine from a number of employees in an office, in which category the office belongs to  
+        '''
+        #we split on the label which is from type "10-19" OR 10000+
+        splitted_label = row['label'].split('-')
+        if len(splitted_label) == 1: #10000+
+            value = math.inf if which == 'end_effectif' else 10000
+        else:
+            if which == 'start_effectif':
+                value = int(splitted_label[0])
+            else:
+                value = int(splitted_label[1])
+
+        return value
+
+    df = load_pd_dataframe("helpers/effectif_labels.csv", ',', dtype={'code':str})
+    df['start_effectif'] = df.apply(lambda row: create_column(row,'start_effectif'), axis=1)
+    df['end_effectif'] = df.apply(lambda row: create_column(row,'end_effectif'), axis=1)
+
+    return df
+
+
 OGR_ROME_CODES = load_ogr_rome_mapping()
 ROME_CODES = list(OGR_ROME_CODES.values())
+
