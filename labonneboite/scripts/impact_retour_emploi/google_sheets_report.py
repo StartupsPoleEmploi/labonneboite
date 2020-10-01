f143c4ddfaea3c913ddfa26c017f65ffb59b10e2
jimmydore <jimmy291295@gmail.com>
2020-10-01 15:13:57 +0200

    Fix set end cell for google sheets in case of more than 26 columns

--- labonneboite/scripts/impact_retour_emploi/google_sheets_report.py
+++ labonneboite/scripts/impact_retour_emploi/google_sheets_report.py
@@ -62,7 +62,17 @@ class GoogleSheetReport:
         letter_first_column = start_cell[0]
         number_first_row = start_cell[1]
 
-        letter_last_column = string.ascii_uppercase[string.ascii_uppercase.index(letter_first_column) + nb_columns - 1]
+        index_last_column = string.ascii_uppercase.index(letter_first_column) + nb_columns - 1
+
+        NUMBER_ALPHABET_LETTERS = len(string.ascii_uppercase) #26 letters in alphabet, we never know, if it changes one day...
+        if index_last_column >= NUMBER_ALPHABET_LETTERS:
+            # It means there are more than 26 columns to insert, then we have to move to the AA column on google sheets
+            first_letter_last_column = string.ascii_uppercase[int(index_last_column/NUMBER_ALPHABET_LETTERS)-1]
+            second_letter_last_column = string.ascii_uppercase[(index_last_column%NUMBER_ALPHABET_LETTERS)]
+            letter_last_column = f"{first_letter_last_column}{second_letter_last_column}"
+        else:
+            letter_last_column = string.ascii_uppercase[index_last_column]
+
         number_last_row = str(int(number_first_row) + nb_rows - 1)
 
         end_cell = f'{letter_last_column}{number_last_row}'
@@ -98,3 +108,15 @@ class GoogleSheetReport:
         )
 
         request.execute()
+
+if __name__ == '__main__':
+    # Never supposed to be called as main
+    # Just for development purposes debug
+    service = generate_google_sheet_service()
+    first_sheet_report = GoogleSheetReport(
+        service=service,
+        spreadsheet_id="toto",
+        sheet_index=0,
+        start_cell='B6',
+        values={"values":[[i for i in range(0,30)]]}
+    )
