b6feab4bc51b3bd62ad967fd123f3f8a6daf617a
Selim Dahmani <selim.dahmani-ext@pole-emploi.fr>
2020-11-09 18:54:50 +0100

    add model for blacklist history

--- labonneboite/common/models/__init__.py
+++ labonneboite/common/models/__init__.py
@@ -25,4 +25,5 @@ from labonneboite.common.models.office import *
 from labonneboite.common.models.auth import *
 from labonneboite.common.models.user_favorite_offices import *
 from labonneboite.common.models.recruiter_message import *
+from labonneboite.common.models.history_blacklist import *
 # pylint: enable=wildcard-import
