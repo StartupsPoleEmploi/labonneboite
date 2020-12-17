f2ab8a5130f016f06653f6f3c43ad015e5eb7716
Alex Hoyau <a.hoyau@silexlabs.org>
2020-12-17 16:53:06 +0100

    tooltip

--- labonneboite/web/static/js/tooltip.js
+++ labonneboite/web/static/js/tooltip.js
@@ -3,5 +3,6 @@
 (function($) {
   $(document).on('lbbready', function () {
     $('[data-toggle="tooltip"]').tooltip();  // Activate tooltips site wide.
+    $('[data-toggle="tooltip"][data-show]').tooltip('show');  // Activate tooltips site wide.
   });
 })(jQuery);
