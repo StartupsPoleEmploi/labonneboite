20dd1b7b1e0f38a4b537c4c1080195d9ffa59b23
Alex Hoyau <a.hoyau@silexlabs.org>
2020-12-18 11:35:57 +0100

    ui related romes

--- labonneboite/web/search/views.py
+++ labonneboite/web/search/views.py
@@ -306,6 +306,7 @@ def entreprises():
         # sort and limit size
         related_romes.sort(key=lambda rome_: rome_.get('score'))
         related_romes = related_romes[:settings.MAX_RELATED_ROMES]
+        flash('Nouvelle fonctionnalité: grâce aux nouveaux filtres, élargissez votre recherche aux métiers qui recrutent !', 'success')
 
 
     # Build form
