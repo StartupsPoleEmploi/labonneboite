dfa2404a4f1ad9013ba5cf98c591f9acfcdfa0eb
Selim Dahmani <selim.dahmani-ext@pole-emploi.fr>
2021-04-23 16:08:57 +0200

    fix sql query

--- labonneboite/scripts/impact_retour_emploi/join_activity_logs_dpae.py
+++ labonneboite/scripts/impact_retour_emploi/join_activity_logs_dpae.py
@@ -54,7 +54,7 @@ class JoinActivityLogsDPAE:
 
     def get_logs_activities_by_sirets(self, sirets):
         engine = import_util.create_sqlalchemy_engine()
-        query = "select * from logs_activity where siret in %s" % tuple(sirets)
+        query = "select * from logs_activity where siret in %s" % str(tuple(sirets))
         df_activity = pd.read_sql_query(query, engine)
         engine.close()
         return df_activity.astype(str)
