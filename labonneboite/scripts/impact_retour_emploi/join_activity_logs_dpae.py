2afae02d797d23fa01b3aff16f7d52f0fae10ceb
Selim Dahmani <selim.dahmani-ext@pole-emploi.fr>
2021-04-23 15:25:33 +0200

    change chuncksize of df csv dpae

--- labonneboite/scripts/impact_retour_emploi/join_activity_logs_dpae.py
+++ labonneboite/scripts/impact_retour_emploi/join_activity_logs_dpae.py
@@ -130,7 +130,7 @@ class JoinActivityLogsDPAE:
         if DEBUG:
             chunksize = 10 ** 2
         else:
-            chunksize = 10000
+            chunksize = 100
 
         i = 0
         column_names = ['kc_siret', 'dc_naf_id', 'dc_adresse', 'dc_codepostal', '_',
