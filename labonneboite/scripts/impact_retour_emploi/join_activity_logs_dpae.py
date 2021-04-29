d93e9b03d737e9924852de01beb4b0789e3a0e91
Selim Dahmani <selim.dahmani-ext@pole-emploi.fr>
2021-04-29 15:58:40 +0200

    change chunksize

--- labonneboite/scripts/impact_retour_emploi/join_activity_logs_dpae.py
+++ labonneboite/scripts/impact_retour_emploi/join_activity_logs_dpae.py
@@ -139,7 +139,7 @@ class JoinActivityLogsDPAE:
         if DEBUG:
             chunksize = 10 ** 2
         else:
-            chunksize = 10000
+            chunksize = 50000
 
         i = 0
         column_names = ['kc_siret', 'dc_naf_id', 'dc_adresse', 'dc_codepostal', '_',
