f78591e269cb9e9a8e54e1ac26ab511eb3fa0055
Alex Hoyau <a.hoyau@silexlabs.org>
2021-05-25 15:59:37 +0200

    Déplacer le bandeau sous la carte et en changer sa couleur

--- labonneboite/web/search/views.py
+++ labonneboite/web/search/views.py
@@ -321,8 +321,6 @@ def entreprises():
                         # sort and limit size
                         related_romes.sort(key=lambda rome_: rome_.get('score'))
                         related_romes = related_romes[:settings.MAX_RELATED_ROMES]
-                        if (len(related_romes) > 0):
-                            flash('Nouvelle fonctionnalité : Grâce aux nouveaux filtres, élargissez votre recherche aux métiers qui recrutent !', 'info')
     else:
         related_romes = []
         hide_suggestions = False
