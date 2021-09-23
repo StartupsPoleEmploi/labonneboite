a0e9422914fbdc2b2ce0ec59ae51e5542ed7eadf
Alex Hoyau <a.hoyau@silexlabs.org>
2021-09-23 09:49:01 +0200

    remove maps

--- labonneboite/web/health/util.py
+++ labonneboite/web/health/util.py
@@ -1,7 +1,6 @@
 import logging
 from labonneboite.common.database import db_session
 from labonneboite.common.es import Elasticsearch
-from labonneboite.common.maps.vendors import ign
 
 logger = logging.getLogger('main')
 
@@ -34,34 +33,3 @@ def is_uwsgi_alive():
     """
     return True
 
-
-def is_ign_duration_alive():
-    endpoint = 'itineraire/rest/route.json'
-    params = {
-        'destination': '6.1697400,49.1080000',
-        'graphName': 'Voiture',
-        'origin': '6.1760260,49.1191460',
-    }
-
-    try:
-        return ign.request_json_api(endpoint, params, timeout=ign.REQUEST_TIMEOUT_SECONDS)["status"] == 'OK'
-    # pylint: disable=W0703
-    except Exception as e:
-        logger.exception(e)
-        return False
-
-
-def is_ign_isochrone_alive():
-    endpoint = 'isochrone/isochrone.json'
-    params = {
-        'location': '4.2645464,48.5300431',
-        'smoothing': 'true',
-        'time': 1800,
-    }
-
-    try:
-        return ign.request_json_api(endpoint, params, timeout=ign.REQUEST_TIMEOUT_SECONDS)["status"] == 'OK'
-    # pylint: disable=W0703
-    except Exception as e:
-        logger.exception(e)
-        return False
