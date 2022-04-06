6aaae2ae9402952c63f878ca30c6093881959980
ESDA2930 <selim.dahmani-ext@pole-emploi.fr>
2022-04-06 10:16:45 +0200

    romve score_alternance_regr field for inserting in etablissement table

--- labonneboite/importer/util.py
+++ labonneboite/importer/util.py
@@ -250,7 +250,7 @@ def get_select_fields_for_backoffice():
     fields += ", effectif, score_regr"
     for i in range(7, 0, -1):  # [7, 6, 5, 4, 3, 2, 1]
         fields += ", `dpae-period-%s`" % i
-    fields += ", score_alternance, score_alternance_regr"
+    fields += ", score_alternance"
     for i in range(7, 0, -1):  # [7, 6, 5, 4, 3, 2, 1]
         fields += ", `alt-period-%s`" % i
     fields += ', flag_poe_afpr, flag_pmsmp'
