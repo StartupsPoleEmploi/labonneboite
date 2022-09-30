f0cccc54c476bdcfad6e6505295964da08d01ed7
sylvain.touret <sylvain.touret@delia.tech>
2022-09-30 16:17:52 +0200

    working version of lbb for dev

--- labonneboite/run.sh
+++ labonneboite/run.sh
@@ -1,3 +1,22 @@
 # /bin/bash
-alembic upgrade head 
-gunicorn --config python:wsgi-conf web.app:app
+
+# alembic
+poetry run alembic upgrade head 
+
+# run custom sql scripts if any
+echo 'Running sql scripts if any'
+for i in `/bin/ls -1 /sql/*.sql`; do 
+    echo $i
+    mysql --user=$DB_USER \
+    --password=$DB_PASSWORD \
+    --host=$DB_HOST \
+    --port=$DB_PORT \
+    --database=$DB_DATABASE < $i
+done
+
+
+# create the index in elastic search
+poetry run create_index --full
+
+# run the server
+poetry run gunicorn --config python:wsgi-conf web.app:app
