9522aa9b0e708b3b80763a7f562d952e46523eb4
sylvain.touret <sylvain.touret@delia.tech>
2022-10-05 16:23:33 +0200

    final update before merging \o/

--- docker/v3.10/entrypoints/test.sh
+++ docker/v3.10/entrypoints/test.sh
@@ -29,6 +29,8 @@ poetry run coverage xml
 poetry build
 
 # prepare test results
+echo "Moving test results file..."
 mkdir -p testResults
 mv *.xml  ./testResults
 mv *.html  ./testResults
+echo "Done"
