24736cdc44b2ceb8511f00205501b6d69d1f928f
Léni Marvaud <24732919+lmarvaud@users.noreply.github.com>
2022-10-31 10:09:42 +0100

    :white_check_mark: fix tests

--- docker/v3.10/entrypoints/test.sh
+++ docker/v3.10/entrypoints/test.sh
@@ -10,28 +10,14 @@ failed() {
 set -ex
 
 # alembic
-poetry run alembic -c labonneboite/alembic.ini upgrade head 
-
-# run custom sql scripts if any
-echo 'Running sql scripts if any'
-for i in `/bin/ls -1 /sql/*.sql`; do 
-    echo $i
-    mysql --user=$DB_USER \
-        --password=$DB_PASSWORD \
-        --host=$DB_HOST \
-        --port=$DB_PORT \
-        --database=$DB_NAME < $i
-done
-
-# create the index in elastic search
-poetry run create_index --full
+poetry run alembic -c labonneboite/alembic.ini upgrade head
 
 # run the tests
 # -- lint
 if ! poetry run flake8 --output-file flake8.txt; then
     failed "flake"
     poetry run flake8_junit flake8.txt flake8.xml && echo
-rm flake8.txt
+    rm flake8.txt
 fi
 
 # -- unit test & coverage
